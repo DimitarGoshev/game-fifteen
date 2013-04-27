e610bec56c0157b886ad433104a86e3171388ef6
GeorgiYolovski <blueforeverabv@gmail.com>
2013-04-27 21:22:31 +0300

    Added class Player

---
+++ Game-Fifteen-3/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameFifteen
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public int Score { get; private set; }
+
+        public Player(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+    }
+}
