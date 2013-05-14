b84ca2c5800b31440118e6397a2b6ecf0fa29bab
Georgi Georgiev <gg3orgiev@gmail.com>
2013-05-15 01:19:54 +0300

    ExecuteGame.cs.cs uncommited in the last commit for unkown reasons ?

---
+++ Game-Fifteen-3/ExecuteGame.cs
@@ -0,0 +1,25 @@
+﻿// **************************
+//
+//  Written by Team "Gold"
+//  Copyright (c) 2012-2013, Telerik Academy
+//
+// **************************
+
+using System;
+using System.Linq;
+
+namespace GameFifteen
+{
+    /// <summary>
+    /// Represents a class that 
+    /// starts the game.
+    /// </summary>
+    public class ExecuteGame
+    {
+        public static void Main(string[] args)
+        {
+            GameEngine game = new GameEngine();
+            game.StartGame();
+        }
+    }
+}
---
+++ Game-Fifteen-3/TopScore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GameFifteen
+{
+    /// <summary>
+    /// Represents a class that provides information
+    /// by Score,
+    /// about Players who have finished the game successfully.
+    /// </summary>
+    public static class TopScore
+    {
+        public static readonly int ScoreListSize = 5;
+        public static List<Player> TopPlayers = new List<Player>();
+
+        /// <summary>
+        /// Represents a method that 
+        /// adds a <seealso cref="Player.cs"/> name to the list of Top Scores.
+        /// </summary>
+        /// <param name="player">The player information that will be written to the lsit</param>
+        /// <param name="position">The position of the player to be added at.</param>
+        public static void AddPlayer(Player player, int position)
+        {
+            TopPlayers.Insert(position, player);
+        }    
+
+        /// <summary>
+        /// Represents a method that prints on the console
+        /// the list of Top Scores if it is not empty.
+        /// </summary>
+        public static void PrintTopScores()
+        {
+            int countPlayers = 0;
+
+            if (TopPlayers.Count == 0)
+            {
+                Console.WriteLine("Top score list is empty.");
+            }
+            else
+            {
+                for (int i = 0; i < TopPlayers.Count; i++)
+                {
+                    Console.WriteLine("Name : {0}, Solved in : {1} moves!",
+                    TopPlayers[i].Name,
+                    TopPlayers[i].Score);
+
+                    if (countPlayers == ScoreListSize)
+                    {
+                        break;
+                    }
+
+                    countPlayers++;
+                }
+            }
+        }
+    }
+}
